7d58a0c4875bb8dc0368c066d027961f7ff9c8ac
Juan Balbuena <hello@juanbalbuena.info>
2017-10-15 16:24:36 +0200

    Add files via upload
    Added part gif

---
+++ js/app.js
@@ -0,0 +1,12 @@
+(function(){
+
+    setTimeout( function() {
+
+        var f = $("#gif");
+        // var col = f.style;
+        f.slideDown("slow");
+        // coloret.fadeIn("slow");
+
+    }, 12200 );
+
+})();
